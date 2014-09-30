c93fcdfa965be6ed58f96b357cebaf2975fbff0e
Ruben Hernando <rhernando@informa.es>
2014-09-30 15:06:55 +0200

    clean

--- resources/public/js/out/om_tools/mixin.js
+++
@@ -1,7 +0,0 @@
-// Compiled by ClojureScript 0.0-2322
-goog.provide('om_tools.mixin');
-goog.require('cljs.core');
-goog.require('om.core');
-goog.require('om.core');
-
-//# sourceMappingURL=mixin.js.map
