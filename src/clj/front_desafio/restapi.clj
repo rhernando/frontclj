b8c12e3349fe7bcd302d4cac97780df15d219841
R Hernando <hernando.ruben@gmail.com>
2014-10-15 01:25:03 +0200

    llamada /users

--- src/clj/front_desafio/restapi.clj
+++ src/clj/front_desafio/restapi.clj
@@ -83,3 +83,19 @@
      (json/read-str body :key-fn keyword)
      (catch Exception e {:message "Error en el servidor"} ))
     ))
+
+(defn friends-data
+  [token]
+  (let [options {:form-params {:auth_token token}
+                 :content-type :json
+                 :accept :json
+                 :headers {"Accept" "application/json"}}
+        {:keys [status error body]} @(http/get (str rest-server "/selecciones/usuarios" ) options)]
+
+    (if error
+      (println "Failed, exception is " error)
+      (println "Async HTTP POST: " status))
+    (try
+     (json/read-str body :key-fn keyword)
+     (catch Exception e {:message "Error en el servidor"} ))
+    ))
